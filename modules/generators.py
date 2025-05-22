b96469a78ac7dc943f4046794bb4651f07c2e1e2
Jorge Magno <jorgejapao00@gmail.com>
2025-05-22 18:12:37 -0300

    character generator command added to modules

---
+++ modules/generators.py
@@ -0,0 +1,24 @@
+from discord.ext import commands
+from discord import app_commands
+from discord.ext.commands import Context
+from discord import Interaction
+from lists.character_generator import *
+import random
+
+
+class CharacterCreator():
+    @staticmethod
+    async def modernCharacter(ctx):
+        idade = random.randint(14,60)
+        jobb = random.choice(Modern.menos25) if idade <= 25 else random.choice(Modern.mais26)
+        tipos = random.choice(sangue) + random.choice(sanguemm)
+        persona= random.choice(personalidades)
+        await ctx.send(f'`Nome:{random.choice(nomesh)} {random.choice(sobrenomeh)}\nIdade: {idade}\nTipo Sanguíneo: {tipos}\nOcupação: {jobb}\nPersonalidade: {persona}`')
+
+    @staticmethod
+    async def medievalCharacter(ctx):
+        idade = random.randint(20,60)
+        jobb = random.choice(Medieval.menos25) if idade <= 25 else random.choice(Medieval.mais26)
+        tipos = random.choice(sangue) + random.choice(sanguemm)
+        persona= random.choice(personalidades)
+        await ctx.send(f'`Nome:{random.choice(nomesh)} {random.choice(sobrenomeh)}\nIdade: {idade}\nTipo Sanguíneo: {tipos}\nOcupação: {jobb}\nPersonalidade: {persona}`')
