a7afb57404934c9a89fd7e85f7ae6085ba51a237
Jorge Magno <jorgejapao00@gmail.com>
2025-05-22 18:13:26 -0300

    character data list added

---
+++ lists/character_generator.py
@@ -0,0 +1,115 @@
+nomesh = [
+  "Naru",
+  "Oye",
+  "Igarashi",
+  "Asai",
+  "Matsushita",
+  "Yashiro",
+  "Oki",
+  "Murata",
+  "Oogami",
+  "Uchibayase",
+  "Sakiyama",
+  "Akibara",
+  "Miyamoto",
+  "Ouji",
+  "Okumura",
+  "Wakatanabe",
+  "Miyasato",
+  "Takamori",
+  "Takamoto",
+  "Mizutani",
+]
+sobrenomeh = [
+  "Kanbe",
+  "Gyokusho",
+  "Susumo",
+  "Kojiro",
+  "Norihide",
+  "Sasekien",
+  "Matsudarai",
+  "Ryoko",
+  "Masanori",
+  "Kazuhiro",
+  "Danjuro",
+  "Mamoru",
+  "Seishiro",
+  "Tomoeio",
+  "Kenji",
+  "Muneyaki",
+  "Takamasa",
+  "Kazuhiko",
+  "Mitsunari",
+  "Yasuhito",
+]
+
+sangue = [
+  'A',
+  'B',
+  'O',
+  'AB'
+]
+sanguemm = [
+  '+',
+  '-'
+]
+personalidades = [
+  'Lógico',
+  'Estratégico',
+  'Independete',
+  'Reservado',
+  'Adaptável',
+  'Criativo',
+  'Líder',
+  'Sociável',
+  'Comandante',
+  'Confiante',
+  'Explosivo',
+  'Cooperativo',
+  'Aventureiro',
+  'Observador'
+]
+
+class Modern():
+    menos25 = [
+    "Estudante",
+    "Entrgador",
+    "Estudante de Intercâmbio",
+    "Attendente",
+    "Garçom",
+    "Vendedor",
+    "Estudante de Artes",
+    "Atleta",
+    "Capitão do Time de Futebol",
+    "Presidente do Conselho Estudantil",
+    "Estudante Transferido"
+    ]
+    mais26 = [
+    'Médico',
+    'Day Trader',
+    'Advogado',
+    'Professor',
+    "Engenheiro Civil",
+    "Policial",
+    "Detetive",
+    "Construtor",
+    "Assistente de Construção",
+    "Programador"
+    ]
+
+class Medieval():
+    menos25 = [
+    'Cavaleiro em Treinamento',
+    'Arqueiro em Treinamento',
+    'Escravo',
+    'Aprendiz de Feiriceiro',
+    'Aprendiz de Curandeiro',
+    'Estudante na academia de artes marciais'
+    ]
+    mais26 = [
+    'Cavaleiro Real',
+    'Arqueiro',
+    'Espadachim',
+    'Mago/Bruxa',
+    'Curandeira(o)'
+    ]
