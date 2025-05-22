581879344e4ce7acab4fcd687b6f5387cedcdf8f
Jorge Magno <jorgejapao00@gmail.com>
2025-05-22 15:47:37 -0300

    Rolls command updated

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from key import key
 from lists import *
 from embeds import *
 from modules.info import *
+from modules.rolls import *
 
 
 os.chdir("D:\\Users\\Eu\\Desktop\\(Audiovisual)\\Argo-Bot")
@@ -81,53 +82,13 @@ async def l(ctx):
 
 @client.command(aliases=['r', 'dado', 'dice'])
 async def roll(ctx, info, conta='', ficha=''):
-  quantidade, numero = info.split('d')
-  nquantidade = int(quantidade)
-  if nquantidade > 1:
-    resultados=[]
-    for i in range (nquantidade):
-      rolagem=random.randint(1,int(numero))
-      resultados.append(rolagem)
-    await ctx.send(f'{ctx.author.mention} 🎇 \n**Rolagem de**: {quantidade}d{numero}\n**Total**: {sum(resultados)}\n**Resultados**: {resultados}')
-  else:
-    rolagem = random.randint(1,int(numero))
-    if conta == '':
-          total = (int(rolagem))
-    elif conta == '+':
-          total = (int(rolagem) + int(ficha))
-    elif conta == '-':
-          total = (int(rolagem) - int(ficha))
-    elif conta == 'x':
-          total = (int(rolagem) * int(ficha))
-    elif conta == '/':
-          total = (int(rolagem) / int(ficha))
-    if ficha == '':
-            ficha = ''
-    if total < 1:
-            total = '1'
-    if rolagem == 20:
-            rolagem = '**20**'
-    if rolagem == 1:
-            rolagem = '**1**'
-    await ctx.send(f'{ctx.author.mention} 🎇 \n**Resultado**: D{numero} ({rolagem}) {conta} {ficha}\n**Total**: {total}')
-
-
-@client.command(aliases=['m', 'mr'])
-async def multiroll(ctx, quantidade=1, numero=20):
-  resultados=[]
-  for i in range (quantidade):
-    rolagem=random.randint(1,int(numero))
-    resultados.append(rolagem)
-  await ctx.send(f'{ctx.author.mention} 🎇 \n**Rolagem de**: {quantidade}d{numero}\n**Resultados**: {resultados}')
+  await Dice.roll(ctx, info, conta, ficha)
 
 
 
 @client.command()
 async def moeda(ctx):
-  await ctx.send('E o resultado é...')
-  await ctx.send('...')
-  await ctx.send(random.choice(cara_coroa))
-
+  await Coin.moeda(ctx)
 
 
 @client.command(aliases=['quantia', 'a'])
