8649b1fa623aa44f1be65a3b337806224fa0b13d
Jorge Magno <jorgejapao00@gmail.com>
2025-05-26 20:17:51 -0300

    quotes commands added to modules

---
+++ modules/phrases.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+from discord.ext.commands import Context
+from discord import Interaction
+from discord import Embed
+from lists.phrases import *
+import random
+
+
+
+class Quote():
+    async def maha(ctx):
+        await ctx.send(random.choice(mahaa))
+
+    async def qotd(ctx):
+        await ctx.send(random.choice(qotd))
+
