eb9757ab6f826fc3790de0ebac357cea74fb0ee8
Jorge Magno <jorgejapao00@gmail.com>
2025-07-24 22:04:52 -0300

    info functions changed to return strings

--- modules/info.py
+++ modules/info.py
@@ -11,22 +11,22 @@ class Info():
     @staticmethod
     async def log(ctx):
         try:
-            await ctx.send(log_file)
+            return(log_file)
         except:
-            await ctx.send('Ops! Algum erro ocorreu enquanto eu tentava te mandar as novidades! Tenta de novo mais tarde!')
+            return('Ops! Algum erro ocorreu enquanto eu tentava te mandar as novidades! Tenta de novo mais tarde!')
 
     @staticmethod
     async def servidor(ctx):
-        await ctx.send(f'Sinta-se livre para entrar no meu servidor oficial, {ctx.author.mention}!\nAqui está o link: https://discord.gg/t2HBP7q37s')
+        return(f'Sinta-se livre para entrar no meu servidor oficial, {ctx.author.mention}!\nAqui está o link: https://discord.gg/t2HBP7q37s')
 
     @staticmethod
     async def version(ctx):
-        await ctx.send(f'Atualmente me encontro na versão {versao}.')
+        return(f'Atualmente me encontro na versão {versao}.')
 
     @staticmethod
     async def l(ctx):
-        await ctx.send("I lived, Bithces.")
+        return("I lived, Bithces.")
 
     @staticmethod
     async def sugestao(ctx):
-        await ctx.send(f'{ctx.author.mention}, você pode enviar uma sugestão de comando por este link: https://bit.ly/3uwBLfb')
+        return(f'{ctx.author.mention}, você pode enviar uma sugestão de comando por este link: https://bit.ly/3uwBLfb')
