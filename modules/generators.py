010c4cf742a6f66b30a7e1c972f307cf4a988278
Jorge Magno <jorgejapao00@gmail.com>
2025-07-24 22:06:13 -0300

    generators functions changed to return strings

--- modules/generators.py
+++ modules/generators.py
@@ -13,7 +13,7 @@ class CharacterCreator():
         jobb = random.choice(Modern.menos25) if idade <= 25 else random.choice(Modern.mais26)
         tipos = random.choice(sangue) + random.choice(sanguemm)
         persona= random.choice(personalidades)
-        await ctx.send(f'`Nome:{random.choice(nomesh)} {random.choice(sobrenomeh)}\nIdade: {idade}\nTipo Sanguíneo: {tipos}\nOcupação: {jobb}\nPersonalidade: {persona}`')
+        return(f'`Nome:{random.choice(nomesh)} {random.choice(sobrenomeh)}\nIdade: {idade}\nTipo Sanguíneo: {tipos}\nOcupação: {jobb}\nPersonalidade: {persona}`')
 
     @staticmethod
     async def medievalCharacter(ctx):
@@ -21,4 +21,4 @@ class CharacterCreator():
         jobb = random.choice(Medieval.menos25) if idade <= 25 else random.choice(Medieval.mais26)
         tipos = random.choice(sangue) + random.choice(sanguemm)
         persona= random.choice(personalidades)
-        await ctx.send(f'`Nome:{random.choice(nomesh)} {random.choice(sobrenomeh)}\nIdade: {idade}\nTipo Sanguíneo: {tipos}\nOcupação: {jobb}\nPersonalidade: {persona}`')
+        return(f'`Nome:{random.choice(nomesh)} {random.choice(sobrenomeh)}\nIdade: {idade}\nTipo Sanguíneo: {tipos}\nOcupação: {jobb}\nPersonalidade: {persona}`')
